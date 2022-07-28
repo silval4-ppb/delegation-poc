cabde84e98ac6adbf109e0eae75632be0ecd264b
Diogo Cardoso <diogo.cardoso@blip.pt>
2022-07-28 10:12:07 +0100

    Fix schema delegation

--- member-api/src/index.ts
+++ member-api/src/index.ts
@@ -1,53 +1,68 @@
 import { ApolloServer } from "apollo-server";
 
 const members = [
-    {
-        id: "1",
-        name: "Luca",
-    },
-    {
-        id: "2",
-        name: "Lara",
-    },
-    {
-        id: "3",
-        name: "Maia",
-    },
-    {
-        id: "4",
-        name: "Rui",
-    },
+  {
+    id: "1",
+    teamId: "1",
+    name: "Luca",
+  },
+  {
+    id: "2",
+    teamId: "1",
+    name: "Lara",
+  },
+  {
+    id: "3",
+    teamId: "2",
+    name: "Maia",
+  },
+  {
+    id: "4",
+    teamId: "2",
+    name: "Rui",
+  },
 ];
 
 const schema = `
     type Member {
-        id: ID!
-        name: String!
+      id: ID!
+      name: String!
     }
 
     type Query {
-        membersById(id: ID!): Member
+      memberById(id: ID!): Member
+      membersByTeam(teamId: ID!): [Member]
     }
-`;
+  `;
 
 const resolvers = {
-    Query: {
-        membersById: (_, { id }) => {
-            console.log("received id", id);
-            return members.find((member) => member.id === id);
-        },
+  Member: {
+    id: ({ id }, args, context, info) => {
+      return id;
     },
+    name: async ({ id }, args, context, info) => {
+      return members.find((member) => member.id === id).name;
+    },
+  },
+  Query: {
+    memberById: (_, { id }) => {
+      return members.find((member) => member.id === id);
+    },
+    membersByTeam: (_, { teamId }) => {
+      return members.filter((member) => member.teamId === teamId);
+    },
+  },
 };
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
-    typeDefs: schema,
-    resolvers: resolvers,
-    context: {},
+  typeDefs: schema,
+  resolvers: resolvers,
+  context: {},
 });
 
 // The `listen` method launches a web server.
 server.listen({ port: 4001 }).then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀  Server ready at ${url}`);
 });
--- team-api/src/index.ts
+++ team-api/src/index.ts
@@ -3,99 +3,94 @@ import { delegateToSchema } from "@graphql-tools/delegate";
 import { loadSchema } from "@graphql-tools/load";
 import { UrlLoader } from "@graphql-tools/url-loader";
 import { OperationTypeNode, print } from "graphql";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { stitchSchemas } from "@graphql-tools/stitch";
 
-const teams = [
+async function main() {
+  const teams = [
     {
-        id: 1,
-        name: "Greenspot",
-        members: [
-            {
-                id: 1,
-            },
-            {
-                id: 2,
-            },
-        ],
+      id: "1",
+      name: "Greenspot",
     },
     {
-        id: 2,
-        name: "Pixies",
-        members: [
-            {
-                id: 3,
-            },
-            {
-                id: 4,
-            },
-        ],
+      id: "2",
+      name: "Pixies",
     },
-];
+  ];
 
-const schema = `
-    type Member {
+  const teamsSchema = makeExecutableSchema({
+    typeDefs: /* GraphQL */ `
+      type Member {
         id: ID!
-        name: String!
-    }
+      }
 
-    type Team {
+      type Team {
         id: ID!
         name: String!
         members: [Member!]!
-    }
+      }
 
-    type Query {
+      type Query {
         teams: [Team!]!
-    }
-`;
+      }
+    `,
+  });
 
-const resolvers = {
+  const membersSchema = await loadSchema("http://localhost:4001/graphql", {
+    loaders: [new UrlLoader()],
+  });
+
+  const teamsResolvers = {
     Query: {
-        teams: () => {
-            return teams.map((team) => {
-                return { id: team.id };
-            });
-        },
+      teams: () => {
+        return teams.map((team) => {
+          return { id: team.id };
+        });
+      },
     },
     Team: {
-        id: ({ id }, args, context, info) => {
-            return id;
-        },
-        name: ({ id }, args, context, info) => {
-            return teams.filter((team) => team.id === id)[0].name;
-        },
-        members: ({ id }) => {
-            return teams.filter((team) => team.id === id)[0].members;
-        },
+      id: ({ id }, args, context, info) => {
+        return id;
+      },
+      name: ({ id }, args, context, info) => {
+        return teams.filter((team) => team.id === id)[0].name;
+      },
+      members: ({ id }, args, context, info) => {
+        return delegateToSchema({
+          schema: membersSchema,
+          operation: OperationTypeNode.QUERY,
+          fieldName: "membersByTeam",
+          args: {
+            teamId: id,
+          },
+          context,
+          info,
+        });
+      },
     },
-    Member: {
-        id: ({ id }, args, context, info) => {
-            return id;
-        },
-        name: async ({ id }, args, context, info) => {
-            const memberAPISchema = await loadSchema("http://localhost:4001", {
-                loaders: [new UrlLoader()],
-            });
+  };
 
-            return delegateToSchema({
-                schema: memberAPISchema,
-                operation: OperationTypeNode.QUERY,
-                fieldName: "membersById",
-                args: { id },
-                info,
-            });
-        },
-    },
-};
+  // setup subschema configurations
+  const teamsSubschema = { schema: teamsSchema };
+  const membersSubschema = { schema: membersSchema };
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
-    typeDefs: schema,
-    resolvers: resolvers,
+  // build the combined schema
+  const gatewaySchema = stitchSchemas({
+    subschemas: [teamsSubschema, membersSubschema],
+  });
+
+  // The ApolloServer constructor requires two parameters: your schema
+  // definition and your set of resolvers.
+  const server = new ApolloServer({
+    typeDefs: gatewaySchema,
+    resolvers: teamsResolvers,
     context: {},
-});
+  });
 
-// The `listen` method launches a web server.
-server.listen({ port: 4000 }).then(({ url }) => {
+  // The `listen` method launches a web server.
+  server.listen({ port: 4000 }).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
-});
+  });
+}
+
+main();
